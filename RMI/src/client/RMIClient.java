cbb3e5ab62500c36fc4eaaa14ed4523ef3e6f042
Coppini <hozerboy2239@gmail.com>
2018-07-06 18:35:25 -0300

    Arruma os IPs

--- RMI/src/client/RMIClient.java
+++ RMI/src/client/RMIClient.java
@@ -17,7 +17,7 @@ public class RMIClient {
             long start = System.currentTimeMillis();
 
             // Aqui vem o IP do Host do servidor
-            String host = "localhost";
+            String host = "192.168.1.109";
 
             // Porta na qual o host vai deixar o server 'escutando'
             int port = 1099;
@@ -36,18 +36,25 @@ public class RMIClient {
 
 
 
-            List<Pessoa> lista = new ArrayList<>();
-            lista.add(new Pessoa("Guilherme Coppini Pavani", 19, 1.73, 9862951));
-            lista.add(new Pessoa("Silvio Novaes de Lira Junior", 19, 1.74, 9778522));
-            lista.add(new Pessoa("Lucas Bordinhon Capalbo", 19, 1.78, 9877982));
-            Pessoa pessoaDaLista = stub.sendListaPessoa(lista);
-//            String oiDaPessoa = stub.oiDaPessoa(new Pessoa("Guilherme", 19, 1.73));
+//            List<Pessoa> lista = new ArrayList<>();
+//            lista.add(new Pessoa("Guilherme Coppini Pavani", 19, 1.73, 9862951));
+//            lista.add(new Pessoa("Silvio Novaes de Lira Junior", 19, 1.74, 9778522));
+//            lista.add(new Pessoa("Lucas Bordinhon Capalbo", 19, 1.78, 9877982));
+//            Pessoa pessoaDaLista = stub.sendListaPessoa(lista);
+
+            String oiDaPessoa = stub.oiDaPessoa(new Pessoa("Guilherme", 19, 1.73, 9862951));
+
 //            Integer intResponse = stub.sendInteger(10);
+
 //            Long longResponse = stub.sendLong(10L);
+
 //            int stringSize = 2048;
 //            String strResponse = stub.sendString(createString(stringSize)); // mudar tamanho da string
+
 //            Long eightLongsResponse = stub.send8Longs(1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L);
+
 //            stub.voidFunction();
+
 //            Double doubleResponse = stub.sendDouble(10.0);
 
 
